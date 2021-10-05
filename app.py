733a9a55e1ca36bfbbcb1a71ca619636dc6bafee
duddn <herok97>
2021-10-05 14:38:27 +0900

    ff

--- app.py
+++ app.py
@@ -125,5 +125,6 @@ def index():
     return "<h1>Hello!</h1>"
 
 if __name__ == '__main__':
-    from waitress_server import serve
-    serve(app, host="0.0.0.0", port=5000)
+    # from waitress_server import serve
+    # serve(app, host="0.0.0.0", port=5000)
+    pass
