bc7e898e302f52851a3eb584b7031033a3ed8768
duddn <herok97>
2021-10-05 14:25:51 +0900

    ff

--- app.py
+++ app.py
@@ -119,7 +119,11 @@ class ResponseCall(Resource):
 #         return {'code': 200,
 #                 'success': 'accept_call successful'}
 #
+
+@app.route("/")
+def index():
+    return "<h1>Hello!</h1>"
+
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host="0.0.0.0", port=5000)
-    app.run()
