3c94ecb93994939f965997482297a0c017ccfa21
duddn <herok97>
2021-10-09 19:34:02 +0900

    book model 추가

--- models.py
+++ models.py
@@ -13,8 +13,8 @@ class User(db.Model):
 
 class Book(db.Model):
     __tablename__ = 'book'
-    user_id = db.Column(db.String(32), nullable=False)
-    counter_id = db.Column(db.String(32), nullable=False)
+    user_id = db.Column(db.String(32), unique=False, nullable=False)
+    counter_id = db.Column(db.String(32), unique=False, nullable=False)
     name = db.Column(db.String(32), nullable=False, default=counter_id)
     is_marked = db.Column(db.Boolean(), nullable=False, default=False)
     __table_args__ = (
