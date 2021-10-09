af6b93239f7aafafc9a31ac881834162b4c7e315
duddn <herok97>
2021-10-09 15:11:22 +0900

    book model 추가

--- SQL.py
+++ SQL.py
@@ -5,8 +5,11 @@ class SQL():
     def __init__(self, db):
         self.db = db
 
-    def get_all_user(self):
+    def get_all_users(self):
         return self.db.session.query(User.user_id, User.password, User.token).all()
+
+    def get_all_books(self):
+        return self.db.session.query(Book.user_id, Book.counter_id, Book.name, Book.is_marked).all()
 
     def get_token_by_id(self, id):
         return self.db.session.query(User).filter(User.user_id == id).first().token
